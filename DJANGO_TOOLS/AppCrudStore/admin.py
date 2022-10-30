b2dfcda5f4d0cd54437c58bc51fa80fcde02f8d8
FabianTorresCh <fabian.torresch@gmail.com>
2022-10-30 17:15:00 -0500

    2022-10-30 : 1. TODOS LOS ELEMENTOS FUNCIONAN CORRECTAMENTE : 2. SE CREO MODELOS PARA TRATAMIENTO A LA BASE DE DATO SQL, LA BASE DE DATOS SE CONECTA CORRECTAMENTE, SE MODIFICA Y ACTUALIZA DESDE EL ADMINISTRADOR : git status -sgit status -sgit status -s RECOMENDACIONES >> PARA LOS MODELOS QUE SE UTILIZAN EN LA BASE DE DATOS, Las inciales de la clase deben ser Table

--- DJANGO_TOOLS/AppCrudStore/admin.py
+++ DJANGO_TOOLS/AppCrudStore/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+# IMPORTACION DE MODELO
+from AppCrudStore.models import ClassProduct
 
-# Register your models here.
+# -------------------------------------------------
+# Permite la manipuacion de las base de datos por el administrador y demas usuarios
+
+# -------------------------------------------------
+# METODO DE VISUALIZACION DE ELEMNTOS
+
+class ViewClassProduct(admin.ModelAdmin):
+    list_display = ("ProductId","ProductName","ProductDescription","ProductValue","ProductInventory","ProductImage")
+    search_fields = ("ProductId","ProductName")
+    list_filter = ("ProductId","ProductName","ProductValue","ProductInventory",)
+
+
+
+
+# -------------------------------------------------
+# REGISTRO DE MODELO DE PRODUCTO
+
+# MODELO DE PRODUCTOS
+admin.site.register(ClassProduct,ViewClassProduct)
--- DJANGO_TOOLS/AppCrudStore/models.py
+++ DJANGO_TOOLS/AppCrudStore/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+# ---------------------------------------------------------
+# MODELS PRODUCTS
+
+# SE DEFINE COMO ESTRUCTURA DE LA TABLA
+# LAS CLASES CREADAS DEBEN TENER EL INDICATIVO DE **TABLE**
+class ClassProduct(models.Model):
+    ProductId = models.AutoField(primary_key=True, verbose_name="Codigo")
+    ProductName = models.CharField(max_length=50, verbose_name="Nombre")
+    ProductDescription = models.TextField(null=True, verbose_name="Descripcion")
+    ProductValue = models.FloatField(verbose_name="Valor")
+    ProductInventory = models.IntegerField(verbose_name="Inventario")
+    ProductImage = models.ImageField(upload_to="Images/",null=True, verbose_name="Imagen")
+
+    # METODO PARA BORRADO DE IMAGEN
+    def DefDeleteImage(self, using = None, keep_parents=False):
+        self.ProductImage.storage.delete(self.ProductImage.name)
+        super(). delete()
